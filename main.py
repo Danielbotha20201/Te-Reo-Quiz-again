bb24aae7ec9d39786be1a6488e395edb749e0d6d
ShadowRaptor <101303928+Danielbotha20201@users.noreply.github.com>
2022-05-18 03:07:23 +0000

    began coding

---
+++ main.py
@@ -0,0 +1,9 @@
+name = input("Hello, what's your name? ")
+
+b = input(f'Kia ora {name}, Would you like to participate in my Te Reo quiz? ')
+
+if b == "yes" or b == "y": 
+ print("Ok have fun!")
+  
+elif b == "no" or b == "n":
+   exit("Have a good day.")
