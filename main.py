5ee32896331cb6e1856c10b2074d2035bd19c100
ShadowRaptor <101303928+Danielbotha20201@users.noreply.github.com>
2022-06-02 22:35:49 +0000

    added the template for questions

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
-name = input("Hello, what's your name? ")
+name = input("Tena koe, what's your name? ")
 
-b = input(f'Kia ora {name}, Would you like to participate in my Te Reo quiz? ')
+z = input(f'Kia ora {name}, Would you like to participate in my Te Reo quiz? ')
 
-if b == "yes" or b == "y": 
- print("Ok have fun!")
+if z == "yes" or z == "y":
   
-elif b == "no" or b == "n":
-   exit("Have a good day.")
+ Q1 = input("What is the Te reo word for folder? ").lower()
+ if Q1 == "":
+    print("Correct")
+  
+ Q2 = input("What is the Te reo word for? ").lower()
+ if Q2 == "":
+    print("Correct!")
+
+ Q3 = input("What is the Te reo word for? ").lower()
+ if Q3 == "": 
+    print("Correct!")
+
+ Q4 = input("What is the Te reo word for? ").lower()
+ if Q4 == "":
+    print("Correct")
+
+ Q5 = input("What is the Te reo word for? ").lower()
+ if Q5 == "":
+    print("Correct")
+
+ Q6 = input("What is the Te reo word for? ").lower()
+ if Q6 == "":
+    print("Correct")
+  
+ Q7 = input("What is the Te reo word for? ").lower()
+ if Q7 == "":
+    print("Correct!")
+
+ Q8 = input("What is the Te reo word for?  ").lower()
+ if Q8 == "": 
+    print("Correct!")
+
+ Q9 = input("What is the Te reo word for? ").lower()
+ if Q9 == "":
+    print("Correct")
+
+ Q10 = input("What is the Te reo word for? ").lower()
+ if Q10 == "":
+    print("Correct")
